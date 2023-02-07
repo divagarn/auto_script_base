9e0bfb88254074aff7dc9c8ff9000e43a36259a6
Divagar N <50747240+divagarn@users.noreply.github.com>
2023-02-07 12:09:57 +0530

    added top and name changed

--- tested/release_image_4_0.sh
+++ tested/release_image_4_0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #############################################################################################
-#Script Name   :person_follower_base_git_src_pull.sh                                                               
+#Script Name   :release_image_4_0.sh                                                               
 #Author        :DIVAGAR N                                                
 #Email         :n.divagar@mobiveil.co.in                                          
 #############################################################################################
@@ -9,11 +9,12 @@
 # Pull person follower base Docker image
 sudo docker pull nimbushaystack/haystack_noetic_person_follower_base:v0.0
 
+#killing the existing container if it was present already 
 sudo docker stop haystack
 sudo docker rm haystack
 
 # Run a new container from the image and name it
-sudo docker run -it -w /haystack_ws/ -d --env /opt/ros/noetic/setup.bash --privileged --name "haystack" nimbushaystack/haystack_noetic_person_follower_base:v0.0
+sudo docker run -it -w /haystack_ws/ -d --env /opt/ros/noetic/setup.bash --privileged --name "haystack" nimbushaystack/haystack_noetic_person_follower_base:v0.0 top
 
 # Clone a Git repository inside the container
 sudo docker exec -w /haystack_ws haystack git clone https://<userid>:<tokenid>@github.com/haystack-nimbus/src.git -b noetic-main
