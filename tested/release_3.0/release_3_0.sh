a90cf8e018f7caa08cc759e5fa8039de6a57cf03
divagarn <divagar2kn@gmail.com>
2023-02-08 18:33:11 +0530

    added two folder to make docker image from file

---
+++ tested/release_3.0/release_3_0.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#############################################################################################
+#Script Name   :release_image_3_0.sh                                                               
+#Author        :DIVAGAR N                                                
+#Email         :n.divagar@mobiveil.co.in                                          
+#############################################################################################
+
+
+read -p "Enter the name for the New image: " image_name
+read -p "Enter the tag for the New image: " tag
+
+sudo docker build -t  $image_name:$tag .
+
+sudo docker login -u "nimbushaystack" -p "cogniCOGNI1!"
+
+sudo docker push $image_name:$tag
+~                                     
